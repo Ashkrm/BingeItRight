89e9dba1abf00710362ccb98e748f99420768bb4
Ashkrm <68059791+Ashkrm@users.noreply.github.com>
2021-04-21 00:37:21 +0530

    Started up my server on port 3000

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+"/home.html");
+})
+
+app.post("/", function(req, res){
+  var search = req.body.search;
+  console.log(search);
+})
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
