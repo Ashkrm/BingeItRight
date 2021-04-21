6bd22d0940294eb1aa3afe8568b33114bd18cebd
Ashkrm <68059791+Ashkrm@users.noreply.github.com>
2021-04-21 20:35:52 +0530

    Added ejs templating

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const bodyParser = require("body-parser");
 const http = require("https");
 
 const app = express();
+
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname+"/home.html");
+  res.render("home");
 })
 
 app.post("/", function(req, res){
